bf719cddcfb04652015eadd5edc5e8073ece9304
Fatih H <fatih.hsu1907@gmail.com>
2022-04-22 09:56:43 +0300

    Banner Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import Header from './Header';
+import Banner from './Banner';
+
 function App() {
   return (
     <div className="app">
       <Header />
+      <Banner />
     </div>
   );
 }
