6193e508ae469968a07683494ecf95a7df246586
Fatih H <fatih.hsu1907@gmail.com>
2022-04-23 03:59:40 +0300

    Processing the Components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import './App.css';
-import Header from './Header';
-import Banner from './Banner';
+import "./App.css";
+import Header from "./Header";
+import Banner from "./Banner";
 
 function App() {
   return (
