cb33b44bf25be6ab981b6d68d0bfbe363d91a05c
Fatih H <fatih.hsu1907@gmail.com>
2022-04-23 03:57:55 +0300

    Minor changes

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import './Home.css';
-
+import React from "react";
+import "./Home.css";
+import Banner from "./Banner";
+import Card from "./Card";
 function Home() {
   return (
-    <div className="home">
+    <div className='home'>
+      <Banner />
       
+      <div className="home__section">
+        <Card />
+        <Card />
+        <Card />
+      </div>
+
+      <div className="home__section">
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
-  )
+  );
 }
 
 export default Home;
