8708d54983a05712f66a3a966191bf73e8d85328
Fatih H <fatih.hsu1907@gmail.com>
2022-04-23 03:59:54 +0300

    Banner is done

--- src/Banner.js
+++ src/Banner.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import './Banner.css';
-import { Button } from '@mui/material';
+import React, { useState } from "react";
+import "./Banner.css";
+import { Button } from "@mui/material";
+import Search from "./Search";
 
 function Banner() {
-  return (
+  const [showSearch, setShowSearch] = useState(false);
 
-    <div className="banner">
-      <div className="banner__info">
+  return (
+    <div className='banner'>
+      <div className='banner__search'>
+        {showSearch && <Search />}
+        <Button
+          className='banner__searchButton'
+          variant='outlined'
+          onClick={() => setShowSearch(!showSearch)}
+        >
+          {!showSearch ? "Search Dates" : "Hide"}
+        </Button>
+      </div>
+      <div className='banner__info'>
         <h1>Get out and stretch your imagination</h1>
-        <h5>Plan a different kind of getaway to uncover the hidden gems near you.</h5>
-        <Button variant="outlined">Explore Nearby</Button>
+        <h5>
+          Plan a different kind of getaway to uncover the hidden gems near you.
+        </h5>
+        <Button variant='outlined'>Explore Nearby</Button>
       </div>
     </div>
-
-  )
+  );
 }
 
 export default Banner;
