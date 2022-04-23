02fb33a024fe65bd8849931dc8c04087e4d1306a
Fatih H <fatih.hsu1907@gmail.com>
2022-04-23 03:58:25 +0300

    Small UI changes

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import './Header.css';
-import SearchIcon from '@mui/icons-material/Search';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import LanguageIcon from '@mui/icons-material/Language';
-import { Avatar } from '@mui/material';
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import LanguageIcon from "@mui/icons-material/Language";
+import { Avatar } from "@mui/material";
 
 function Header() {
   return (
     <div className="header">
-      <img className="header__icon"
+      <img
+        className="header__icon"
         src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
         alt=""
       />
@@ -25,7 +26,7 @@ function Header() {
         <Avatar />
       </div>
     </div>
-  )
+  );
 }
 
 export default Header;
