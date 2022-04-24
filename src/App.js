4512bc1ae4ff00092271b13e4c8d22373a1f5bef
Fatih H <fatih.hsu1907@gmail.com>
2022-04-24 12:47:23 +0300

    Minor Changes

--- src/App.js
+++ src/App.js
@@ -4,13 +4,28 @@ import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import Footer from "./Footer";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import SearchPage from "./SearchPage";
+
 
 function App() {
+
+
   return (
-    <div className="app">
-      <Header />
-      <Home />
-      <Footer />
+    <div className='app'>
+      <BrowserRouter>
+        <Header />
+        
+        <Routes>
+
+          <Route path='/' element={<Home />}/>
+        
+          <Route path='/search' element={<SearchPage />}/>
+        
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
